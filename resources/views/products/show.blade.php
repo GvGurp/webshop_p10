9c76d2738c308c16976b27bd9ed8e03e5c66dfd5
Gaby <137078206+GvGurp@users.noreply.github.com>
2024-10-01 14:35:43 +0200

    Products model (Gaby)
    Producten model aangemaakt met de gegevens erin.
    
    !!Oude producten mapje verwijderd!!

--- resources/views/products/show.blade.php
+++
@@ -1,3 +0,0 @@
-<h1>{{ $product->name}}</h1>
-<p> Dit product kost{{ $product->price}} euro.</p>
-<p> Dit product is aangemaakt op {{ $product->created_at }}</p>
--- resources/views/webshop/webShop.blade.php
+++ resources/views/webshop/webShop.blade.php
@@ -2,5 +2,6 @@
 
 
 @section('content')
+<h1> Webshop</h1>
 
 @endsection
